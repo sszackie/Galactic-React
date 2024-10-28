3527a147b6f2f6665867601f209ba378c949bed6
sszackie <89492126+sszackie@users.noreply.github.com>
2024-10-28 09:54:53 -0400

    Update chat.js

--- src/views/chat.js
+++ src/views/chat.js
@@ -147,42 +147,54 @@ const Chat = (props) => {
         <br></br>
       </h1>
       <h1>Join Our Live Chat Room</h1>
-      <span className="chat-text47">
-        <span>Join our live chat room on Discord:</span>
-        <br></br>
-      </span>
+      <span className="chat-text47">Join our live chat room on Discord:</span>
+      <svg width="32" height="32" viewBox="0 0 32 32">
+        <path
+          d="M25.7 7.1Q23 5.9 20 5.3h-.1c-.2.4-.5 1-.7 1.5c-2.2-.3-4.3-.3-6.4 0c-.2-.5-.5-1-.7-1.5H12q-3 .45-5.7 1.8C2.7 12.5 1.7 17.8 2.2 23v.1c2.4 1.8 4.7 2.8 7 3.5h.1c.5-.7 1-1.5 1.4-2.3v-.1c-.8-.3-1.5-.6-2.2-1c-.1 0-.1-.1 0-.1c.1-.1.3-.2.4-.3H9c4.6 2.1 9.5 2.1 14.1 0h.1c.1.1.3.2.4.3c.1 0 0 .1 0 .1c-.7.4-1.4.8-2.2 1c0 0-.1.1 0 .1c.4.8.9 1.6 1.4 2.3h.1c2.3-.7 4.6-1.8 7-3.5V23c.6-6-1-11.2-4.2-15.9M11.4 19.9c-1.4 0-2.5-1.3-2.5-2.8s1.1-2.8 2.5-2.8s2.5 1.3 2.5 2.8s-1.1 2.8-2.5 2.8m9.3 0c-1.4 0-2.5-1.3-2.5-2.8s1.1-2.8 2.5-2.8s2.5 1.3 2.5 2.8s-1.1 2.8-2.5 2.8"
+          fill="currentColor"
+        ></path>
+      </svg>
       <a
         href="https://discord.gg/NdBJVXkawb"
         target="_blank"
         rel="noreferrer noopener"
-        className="chat-link"
+        className="chat-link1"
       >
-        Here
+        Support Discord Server
+      </a>
+      <h1>Join Our Partners Discord Server</h1>
+      <a
+        href="https://discord.gg/xpnATXBpaJ"
+        target="_blank"
+        rel="noreferrer noopener"
+        className="chat-link2"
+      >
+        GITE Partner Server (Partnership)
       </a>
       <Footer4
         link1={
           <Fragment>
-            <span className="chat-text50 thq-body-small">Home</span>
+            <span className="chat-text49 thq-body-small">Home</span>
           </Fragment>
         }
         link2={
           <Fragment>
-            <span className="chat-text51 thq-body-small">Games</span>
+            <span className="chat-text50 thq-body-small">Games</span>
           </Fragment>
         }
         link3={
           <Fragment>
-            <span className="chat-text52 thq-body-small">Proxies</span>
+            <span className="chat-text51 thq-body-small">Proxies</span>
           </Fragment>
         }
         link4={
           <Fragment>
-            <span className="chat-text53 thq-body-small">Tools</span>
+            <span className="chat-text52 thq-body-small">Tools</span>
           </Fragment>
         }
         link5={
           <Fragment>
-            <span className="chat-text54 thq-body-small">Settings</span>
+            <span className="chat-text53 thq-body-small">Settings</span>
           </Fragment>
         }
       ></Footer4>
